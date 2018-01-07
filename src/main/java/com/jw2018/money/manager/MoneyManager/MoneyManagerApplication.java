25726798c0239de74f20dc99f41c1412a51c7ab8
JW <35067516JW@users.noreply.github.com>
2018-01-07 20:38:41 +0100

    initial project with controller setup

---
+++ src/main/java/com/jw2018/money/manager/MoneyManager/MoneyManagerApplication.java
@@ -0,0 +1,12 @@
+package com.jw2018.money.manager.MoneyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyManagerApplication.class, args);
+	}
+}
