06f3c42d1d64d2c487771aaa477df4d7874043e3
JW <35067516JW@users.noreply.github.com>
2018-01-07 20:39:04 +0100

    initial project with controller setup

---
+++ src/test/java/com/jw2018/money/manager/MoneyManager/MoneyManagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jw2018.money.manager.MoneyManager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoneyManagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
